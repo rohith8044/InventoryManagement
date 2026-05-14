a3c0e14fbe42f8dc9826700cc7955a95f0524c56
Rohith <your_email@example.com>
2026-05-14 18:45:54 +0530

    f

---
+++ src/main/java/com/example/InventoryManagementSystem/Model/Brand.java
@@ -0,0 +1,28 @@
+package com.example.InventoryManagementSystem.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long brandId;
+
+    private String brandName;
+
+    private String description;
+
+    private String status = "active";
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
