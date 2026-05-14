2aa4189bead812d60eeb07fde73e476acabfbcf5
Rohith <your_email@example.com>
2026-05-14 18:49:53 +0530

    w

--- src/main/java/com/example/InventoryManagementSystem/Model/Brand.java
+++ src/main/java/com/example/InventoryManagementSystem/Model/Brand.java
@@ -16,7 +16,7 @@ public class Brand {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long brandId;
 
-    private String brandNames;
+    private String bran;
 
     private String description;
 
